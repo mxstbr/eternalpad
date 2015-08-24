4afb5e54ebaba70313437c6467ad66c21597f292
Max <contact@mstoiber.com>
2015-08-24 21:24:16 +0200

    Switch to localforage
    Switched to localforage by mozilla for better browser support. Closes #6. More information: https://github.com/mozilla/localForage

--- js/main.js
+++ js/main.js
@@ -1,39 +1,27 @@
-var textarea, localStorageSupported;
+var textarea;
+var localforage = require('localforage');
 
 window.onload = function() {
   // Get textarea and test for localStorage support
   textarea = document.querySelector('.textarea');
-  localStorageSupported = localStorageTest();
 
-  if (localStorageSupported === true) {
-    // Set the initial Content to the previously saved one, or to the default one
-    var initialContent = localStorage.getItem('content') || 'Welcome to Eternalpad.\n\nWrite something here, then refresh the page.\nYour note will still be here, waiting for you.';
-    textarea.value = initialContent;
-    // Save the text onkeyup and onblur
-    textarea.onkeyup = changeContent;
-    textarea.onblur = changeContent;
-    // Focus textarea to open virtual keyboards on touch devices
-    textarea.focus();
-  } else {
-    // If local storage is not supported, display an error message
-    textarea.value = "Sorry, your browser is not supported by Eternalpad. Switch to a modern browser to get full functionality.";
-  }
+  // Set the initial Content to the previously saved one, or to the default one
+  localforage.getItem('content', function(err, value) {
+    if (err) {
+      textarea.value = "Sorry, your browser is not supported by Eternalpad. Switch to a modern browser to get full functionality.";
+    } else {
+      textarea.value = value || 'Welcome to Eternalpad.\n\nWrite something here, then refresh the page.\nYour note will still be here, waiting for you.';
+      // Save the text onkeyup and onblur
+      textarea.onkeyup = changeContent;
+      textarea.onblur = changeContent;
+      // Focus textarea to open virtual keyboards on touch devices
+      textarea.focus();
+    }
+  });
 };
 
 // Saves the content to localStorage and sets the new textarea value
 function changeContent(evt) {
-  localStorage.setItem('content', evt.target.value);
+  localforage.setItem('content', evt.target.value);
   textarea.value = evt.target.value;
-}
-
-// Tests if localStorage is supported, shamelessly stolen from Modernizr: https://github.com/Modernizr/Modernizr/blob/master/feature-detects/storage/localstorage.js
-function localStorageTest() {
-  var test = 'test';
-  try {
-    localStorage.setItem(test, test);
-    localStorage.removeItem(test);
-    return true;
-  } catch (e) {
-    return false;
-  }
 }
